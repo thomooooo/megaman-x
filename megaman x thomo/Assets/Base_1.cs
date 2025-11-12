f5c58c66f6197fc8377a587aa01c756a0299476d
thomooo <tleivatorrealba@gmail.com>
2025-11-12 04:28:12 -0300

    b

---
+++ megaman x thomo/Assets/Base_1.cs
@@ -0,0 +1,174 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Base_1 : MonoBehaviour
+{
+    public float Speed;
+    public float AlturaSalto;
+    public float PotenciaSalto;
+    public float Gravedad;
+    public int Fase1;
+    public int Fase2;
+    public bool Saltando;
+    public float Fallen;
+    public Animator ani;
+    private float YPos;
+    private int sky_;
+
+    ///////////////////Detector de piso///////////////////////
+
+    private RaycastHit2D hit;
+    public Vector3 v3;
+    public float distance;
+    public LayerMask layer;
+
+    /////////////////// JumpWall ///////////////////////
+
+    // Start is called before the first frame update
+
+
+
+    void Start()
+    {
+        ani = GetComponent<Animator>();
+    }
+    void OnDrawGizmos()
+    {
+        Gizmos.DrawRay(transform.position + v3, Vector3.up * -1 * distance);
+    }
+    public bool CheckCollision
+    {
+        get
+        {
+            hit = Physics2D.Raycast(transform.position + v3, transform.up * -1, distance, layer);
+            return hit.collider != null;
+        }
+    }
+
+    public void Detector_Plataforma()
+    {
+        if (CheckCollision)///////******
+        {
+            ani.SetBool("sky", false);
+            sky_ = 0;
+
+            if (!Saltando)
+            {
+                Gravedad = 0;
+                Fase1 = 0;
+                Fase2 = 0;
+            }
+        }
+        else
+        {
+            ani.SetBool("sky", true);
+            if (!Saltando)
+            {
+                switch (Fase2)
+                {
+                    case 0:
+                        Gravedad = 0;
+                        Fase2 = 1;
+                        break;
+                    case 1:
+                        if (Gravedad > -10)
+                        {
+                            Gravedad -= AlturaSalto / Fallen * Time.deltaTime;
+                        }
+                        break;
+                }
+            }
+        }
+
+        if (transform.position.y > YPos)
+        {
+            ani.SetFloat("gravedad", 1);
+        }
+        if (transform.position.y < YPos)
+        {
+            ani.SetFloat("gravedad", 0);
+
+            switch (sky_)
+            {
+                case 0:
+                    ani.Play("Base Layer.Sky", 0, 0);
+                    sky_++;
+                    break;
+            }
+        }
+
+        YPos = transform.position.y;
+    }
+    public void Jump()
+    {
+        if (Input.GetKey(KeyCode.C))
+        {
+            switch (Fase1)
+            {
+                case 0:
+
+                    if (CheckCollision)
+                    {
+                        Gravedad = AlturaSalto;
+                        Fase1 = 1;
+                        Saltando = true;
+                    }
+
+                    break;
+                case 1:
+
+                    if (Gravedad > 0)
+                    {
+                        Gravedad -= PotenciaSalto * Time.deltaTime;
+                    }
+                    else
+                    {
+                        Fase1 = 2;
+                    }
+                    Saltando = true;
+
+                    break;
+                case 2:
+                    Saltando = false;
+                    break;
+            }
+        }
+        else
+        {
+            Saltando = false;
+        }
+    }
+    public void Move()
+    {
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Translate(Vector3.right * Speed * Time.deltaTime);
+            transform.rotation = Quaternion.Euler(0, 0, 0);
+            ani.SetBool("run", true);
+        }
+        else
+        {
+            ani.SetBool("run", false);
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Translate(Vector3.right * Speed * Time.deltaTime);
+            transform.rotation = Quaternion.Euler(0, 180, 0);
+            ani.SetBool("run", true);
+        }
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Move();
+        Jump();
+    }
+    void Update()
+    {
+        Detector_Plataforma();
+        transform.Translate(Vector3.up * Gravedad * Time.deltaTime);
+    }
+}
--- megaman x thomo/Assets/movi8miento.cs
+++ megaman x thomo/Assets/movi8miento.cs
@@ -1,6 +1,7 @@
 using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(Animator))]
 public class MegaManXWallSlide : MonoBehaviour
 {
     [Header("Movimiento")]
@@ -21,74 +22,112 @@ public class MegaManXWallSlide : MonoBehaviour
     public LayerMask groundLayer;
 
     private Rigidbody2D rb;
+    private Animator ani;
+
     private bool isGrounded;
     private bool isTouchingWall;
     private bool isWallSliding;
     private bool isWallJumping;
+    private bool isJumping;
+    private bool isRunning;
+
     private float moveInput;
-    private int facingDirection = 1; // 1 = derecha, -1 = izquierda
+    private float lastNonZeroInput;   // guarda la última dirección válida
+    private float idleBufferTimer;    // pequeño buffer para evitar micro-idle
+    private int facingDirection = 1;
+
+    [Header("Buffer de idle (segundos)")]
+    public float idleBuffer = 0.1f;   // cuanto tiempo espera antes de poner idle
 
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        ani = GetComponent<Animator>();
         wallJumpDirection.Normalize();
     }
 
     private void Update()
     {
-        moveInput = Input.GetAxisRaw("Horizontal");
+        // --- entrada y detección ---
+        moveInput = Input.GetAxis("Horizontal");
         isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
         isTouchingWall = Physics2D.OverlapCircle(wallCheck.position, wallCheckRadius, wallLayer);
 
-        // --- Dirección actual del personaje ---
-        if (moveInput > 0) facingDirection = 1;
-        else if (moveInput < 0) facingDirection = -1;
+        // --- dirección ---
+        if (moveInput > 0.05f) facingDirection = 1;
+        else if (moveInput < -0.05f) facingDirection = -1;
 
-        // --- Movimiento normal ---
+        // --- movimiento horizontal ---
         if (!isWallSliding && !isWallJumping)
         {
             rb.linearVelocity = new Vector2(moveInput * moveSpeed, rb.linearVelocity.y);
         }
 
-        // --- Salto normal ---
+        // --- salto normal ---
         if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
         {
             rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
+            isJumping = true;
+            ani.SetTrigger("jumpTrigger");
         }
 
-        // --- Detección de deslizamiento por pared ---
-        if (isTouchingWall && !isGrounded && moveInput == facingDirection)
-        {
+        if (rb.linearVelocity.y < -0.1f && !isGrounded)
+            isJumping = false;
+
+        // --- pared y salto en pared ---
+        if (isTouchingWall && !isGrounded && Mathf.Sign(moveInput) == facingDirection)
             isWallSliding = true;
-        }
         else
-        {
             isWallSliding = false;
-        }
 
-        // 💡 NUEVO: si presionás en dirección contraria, se despega al instante
-        if (isWallSliding && moveInput == -facingDirection)
-        {
-            isWallSliding = false;
-        }
-
-        // --- Deslizamiento ---
         if (isWallSliding)
-        {
             rb.linearVelocity = new Vector2(0, Mathf.Max(rb.linearVelocity.y, -wallSlideSpeed));
-        }
 
-        // --- Salto desde la pared ---
         if (Input.GetKeyDown(KeyCode.Space) && isWallSliding)
         {
             isWallSliding = false;
             isWallJumping = true;
+            isJumping = true;
 
             Vector2 force = new Vector2(-facingDirection * wallJumpDirection.x, wallJumpDirection.y) * wallJumpForce;
             rb.linearVelocity = Vector2.zero;
             rb.AddForce(force, ForceMode2D.Impulse);
 
-            Invoke(nameof(StopWallJump), 0.2f);
+            ani.SetTrigger("wallJumpTrigger");
+            Invoke(nameof(StopWallJump), 0.25f);
+        }
+
+        // --- manejo del buffer para correr ---
+        if (Mathf.Abs(moveInput) > 0.05f && isGrounded)
+        {
+            isRunning = true;
+            idleBufferTimer = idleBuffer;         // resetea el tiempo de espera
+            lastNonZeroInput = moveInput;         // guarda última dirección
+        }
+        else
+        {
+            if (idleBufferTimer > 0)
+                idleBufferTimer -= Time.deltaTime; // sigue corriendo un poco
+            else
+                isRunning = false;                 // recién ahora pasa a idle
+        }
+
+        // --- animaciones ---
+        ani.SetBool("isRunning", isRunning);
+        ani.SetBool("isGrounded", isGrounded);
+        ani.SetBool("isJumping", isJumping);
+        ani.SetBool("isFalling", rb.linearVelocity.y < -0.1f && !isGrounded);
+        ani.SetBool("isWallSliding", isWallSliding);
+        ani.SetBool("isWallJumping", isWallJumping);
+
+        // --- giro visual ---
+        if (!isWallJumping)
+        {
+            Vector3 s = transform.localScale;
+            if (lastNonZeroInput > 0.05f)
+                transform.localScale = new Vector3(Mathf.Abs(s.x), s.y, s.z);
+            else if (lastNonZeroInput < -0.05f)
+                transform.localScale = new Vector3(-Mathf.Abs(s.x), s.y, s.z);
         }
     }
 
@@ -101,7 +140,6 @@ public class MegaManXWallSlide : MonoBehaviour
     {
         if (wallCheck != null)
             Gizmos.DrawWireSphere(wallCheck.position, wallCheckRadius);
-
         if (groundCheck != null)
             Gizmos.DrawWireSphere(groundCheck.position, groundCheckRadius);
     }
